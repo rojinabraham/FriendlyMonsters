5bbd317658f0449cb31f2100d44fb727db8518d2
Rojin Abraham <rojin.abraham.jb@gmail.com>
2020-04-24 00:40:10 +0530

    Updates

---
+++ precache-manifest.e3d158b3c7460a93426cff8f2e320278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d2c638bd4cceb637c9df2dc3fa0a8e",
+    "url": "/FriendlyMonsters/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/FriendlyMonsters/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "964f1324a92e37bc70e0",
+    "url": "/FriendlyMonsters/static/css/main.f2adfe47.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/FriendlyMonsters/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FriendlyMonsters/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964f1324a92e37bc70e0",
+    "url": "/FriendlyMonsters/static/js/main.512e8af7.chunk.js"
+  },
+  {
+    "revision": "51db5afd468d1f9d1391",
+    "url": "/FriendlyMonsters/static/js/runtime-main.287d9cbe.js"
+  },
+  {
+    "revision": "7592135e24102e21c283175c5560763e",
+    "url": "/FriendlyMonsters/static/media/mrsmonsterexpand.7592135e.woff"
+  }
+]);
